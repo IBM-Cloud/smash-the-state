93d5c0e0eb0b18ef6a81c0432f7d6444f4c7fd8c
Dan Connor <dan@danconnor.com>
2018-02-08 15:02:29 -0500

    add support for definitions in definitions

--- lib/smash_the_state/operation/definition.rb
+++ lib/smash_the_state/operation/definition.rb
@@ -5,11 +5,6 @@ module SmashTheState
       class << self
         attr_reader :schema_block
 
-        # assigns a name to the definition
-        def definition(definition_name)
-          @definition_name = definition_name
-        end
-
         # the "name" is available as a reference
         def ref
           @definition_name
@@ -18,9 +13,21 @@ module SmashTheState
         # whenever this module is evaluated as a string, use its name
         alias to_s ref
 
-        # the provided schema block is both stored for re-use and also evaluated in the
-        # definition module itself
-        def schema(&block)
+        private
+
+        # assigns a name to the definition
+        def definition(definition_name)
+          @definition_name = definition_name
+        end
+
+        def schema(name = nil, options = {}, &block)
+          # if a name is provided, it's an inline schema or a reference to another
+          # definition
+          return super unless name.nil?
+
+          # called with no name, we infer that this is the definition's own schema. the
+          # provided schema block is both stored for re-use and also evaluated in the
+          # definition module itself
           @schema_block = block
           class_eval(&block)
         end
---
+++ spec/unit/operation/definition_spec.rb
@@ -0,0 +1,57 @@
+require "spec_helper"
+
+# not sure why getting a `NoMethodError: undefined method `empty?' for nil:NilClass` error
+# when the literal module is referenced here as opposed to as a string
+describe "SmashTheState::Operation::Definition" do
+  let!(:subject) do
+    location_definition = Class.new(SmashTheState::Operation::Definition).tap do |k|
+      k.class_eval do
+        definition "Location"
+
+        schema do
+          attribute :postal_code, :string
+          attribute :lat, :float
+          attribute :lon, :float
+        end
+      end
+    end
+
+    Class.new(SmashTheState::Operation::Definition).tap do |k|
+      k.class_eval do
+        definition "Syndicate"
+
+        schema do
+          # smoke test nested definitions
+          schema :location, ref: location_definition
+        end
+      end
+    end
+  end
+
+  describe "inheritance" do
+    it "inherits from State" do
+      expect(subject.ancestors).to include(SmashTheState::Operation::State)
+    end
+  end
+
+  describe "#ref" do
+    it "returns the definition name" do
+      expect(subject.ref).to eq("Syndicate")
+    end
+  end
+
+  describe "#to_s" do
+    it "returns the ref" do
+      expect(subject.to_s).to eq(subject.ref)
+    end
+  end
+
+  describe "#schema with a referenced definition" do
+    it "pulls in the schema from the referened definition" do
+      expect(
+        # wheeeeeee
+        subject.attributes_registry[:location][1][:ref].attributes_registry.keys
+      ).to eq([:postal_code, :lat, :lon])
+    end
+  end
+end
